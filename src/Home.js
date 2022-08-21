62e56f2cfdf97b2fadbc08dabd55307e84b476fa
riaz4519 <riaz.i3216@gmail.com>
2022-08-21 22:24:44 +0600

    event tutorial done

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello ninja",e);
+  };
+
+  const handleClickAgain = (name,r) => {
+    console.log("Hello " + name,r.target);
+  };
   return (
     <div className="home">
       <h2>Home page</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(r) => handleClickAgain("marios",r)}>Click me again</button>
     </div>
   );
 };
