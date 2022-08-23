e7094254c0bfb75349bbdedcddd4258f40f76eb1
riaz4519 <riaz.i3216@gmail.com>
2022-08-23 19:15:41 +0600

    on react router 21

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
