ecf2ddd5045dde00a9e588c992645042b52657b1
riaz4519 <riaz.i3216@gmail.com>
2022-08-27 23:04:05 +0600

    react router 23

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Create from "./Create";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
