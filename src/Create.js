b8f2f7481bdc8805856d0a3e16d2f2e36d70bdf2
riaz4519 <riaz.i3216@gmail.com>
2022-08-28 13:36:21 +0600

    submit events done

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("riaz");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const blog = {
+      title,
+      body,
+      author,
+    };
+
+    console.log(blog);
+  };
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title : </label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label>Blog body : </label>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
+
+        <label>Blog Author : </label>
+        <select
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">Mario</option>
+          <option value="riaz">Riaz</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
